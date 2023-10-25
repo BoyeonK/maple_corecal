cc28fe85ba201ac35a7a6e55b4c87b05336ebda1
KBY <tetepiti149@gmail.com>
2023-10-25 19:49:54 +0900

    feat : blockquote 추가

---
+++ src/components/blockqt.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const Blockqt = () => {
+  return (
+    <Block>
+      <ColorBlock/>
+      아무노래나일단틀어
+    </Block>
+  );
+}
+
+const Block = styled.div`
+  display: flex;
+  margin: 50px 10px 10px 10px;
+  height: 50px;
+  width: 100%;
+`
+
+const ColorBlock = styled.div`
+  width: 10px;
+  margin-right: 10px;
+  background: #9d9d9d;
+`
+
+export default Blockqt;
