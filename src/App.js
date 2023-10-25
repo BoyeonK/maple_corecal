21235ef7a6ab219fadba79dbc5484a431b8b7ac4
KBY <tetepiti149@gmail.com>
2023-10-25 15:36:49 +0900

    feat : add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Header from './components/header'
 
 function App() {
   return (
-    <div className="App">
-      <div></div>
-    </div>
+    <Header/>
   );
 }
 
---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <HeaderContents>
+      <h4>asdfasdf</h4>
+    </HeaderContents>
+  );
+}
+
+const HeaderContents = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: black;
+  color: white;
+`
+  
+export default Header;
