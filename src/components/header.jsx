faad5b296257d7516c2b8abbf38755bff7bd2243
KBY <tetepiti149@gmail.com>
2023-10-25 16:00:35 +0900

    feat : add header taps

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,17 +3,28 @@ import styled from "styled-components";
 const Header = () => {
   return (
     <HeaderContents>
-      <h4>asdfasdf</h4>
+      <HeaderTap>강화할 코어</HeaderTap>
+      <HeaderTap>내 코어</HeaderTap>
     </HeaderContents>
   );
 }
 
 const HeaderContents = styled.div`
   display: flex;
+  align-items: center;
+  height: 60px;
+  background: white;
+  color: black;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-bottom: 2px solid #999999;
+`
+
+const HeaderTap = styled.div`
+  display: flex;
   justify-content: center;
   align-items: center;
-  background: black;
-  color: white;
+  width: 200px;
 `
   
 export default Header;
