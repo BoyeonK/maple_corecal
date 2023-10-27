b41ebbe2809a53cba358d1684a33067595de048a
KBY <tetepiti149@gmail.com>
2023-10-27 14:37:15 +0900

    test : 코어 선택 완료 테스트 코드

--- src/components/StepOne/corecompletebtn.jsx
+++ src/components/StepOne/corecompletebtn.jsx
@@ -1,12 +1,32 @@
 import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { SelectedCores } from "../../recoil/Atoms";
 
-const CoreCompleteBtn = () => (
-  <CoreBtnDiv>
-    <CoreToggle>
-      선택 완료
-    </CoreToggle>
-  </CoreBtnDiv>
-);
+import { useSetRecoilState } from "recoil";
+
+const CoreCompleteBtn = () => {
+  const cores = useRecoilValue(SelectedCores);
+  const testtt = useSetRecoilState(SelectedCores)
+  const test = false;
+  const cClick = () => {
+    let i = 0;
+    cores.forEach((e) => { if(e) i += 1; })
+    if (i>2) {
+      testtt([true, true, true, true, true, true, true, true, true, true]);
+    }
+    else {
+      testtt([false,false,false,false,false,false,false,false,false,false,]);
+    }
+  }
+
+  return (
+    <CoreBtnDiv>
+      <CoreToggle test={test} onClick={cClick}>
+        선택 완료
+      </CoreToggle>
+    </CoreBtnDiv>
+  )
+};
 
 const CoreBtnDiv = styled.div`
   width: 100%;
@@ -19,9 +39,10 @@ const CoreToggle = styled.div`
   align-items: center;
   width: 500px;
   height: 50px;
-  margin: auto;
+  margin: ${(props) => (props.test) ? '9px 12px 8px' : 'auto'};
   background: #ccc;
   border-radius: 5px;
+  cursor: pointer;
 `
 
 export default CoreCompleteBtn;
